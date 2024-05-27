2fef36e7599d202c3569d7cb8fd6f613375af08f
MPaz <mbattistoni@frro.utn.edu.ar>
2024-05-27 19:02:16 -0300

    arreglo mio

--- src/shared/db/conn.ts
+++ src/shared/db/conn.ts
@@ -1,8 +1,8 @@
 import { MongoClient, Db} from "mongodb";
 
-const connectionStr = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/'
+const connectionStr = process.env.MONGO_URI || 'mongodb+srv://mariapaz:TrabajoDSW@restaurant.xreyzj3.mongodb.net/'
 
 const cli = new MongoClient(connectionStr)
 await cli.connect()
 
-export let db: Db = cli.db('restaurantDB')
+export let db: Db = cli.db('TrabajoDSW')
