de606376869dc6dd0f8b94fa291a128f2a8d6918
pazbattistoni <140267447+pazbattistoni@users.noreply.github.com>
2024-11-05 13:39:18 -0300

    modificacion entidad product
    agregue lo que faltaba de las categorias, se identifican con id para que sea unico

--- src/Product/product.entity.ts
+++ src/Product/product.entity.ts
@@ -4,10 +4,23 @@ import { Category } from '../Category/category.entity.js'
 import { BaseEntity } from '../shared/db/baseEntity.entity.js'
 
 @Entity()
-export class Product extends BaseEntity{ //faltan los atributos; como lo relaciono con categoria?
+export class Product extends BaseEntity{ //faltan los atributos; como lo relaciono con categoria? avisar si no quedo bien, intente resolverlo
 
-    @Property({nullable: false, unique:true}) // tiene que tener nombre y no se puede repetir
-    name!: string
-    @ManyToOne(() => Category, { nullable: false })
-    category!: Rel<Category>
+    @PrimaryKey()
+    _id!: ObjectId;
+
+    @Property({ nullable: false, unique: true })
+    name!: string;
+
+    @Property({ nullable: false }) 
+    price!: number;
+
+    @Property({ nullable: true }) 
+    description?: string;
+
+    @ManyToOne(() => Category, { nullable: false }) 
+    category!: Rel<Category>;
+
+    @Property({ nullable: true }) 
+    imageUrl?: string;
 }
