a7f3c44bcea5d43789b4d9cf73d8d27ede775863
facundo <cantabertafacu@gmail.com>
2025-08-18 19:02:18 -0300

    one drive deleted

--- OneDrive/Desktop/clases soporte/whatsmsg.py
+++
@@ -1,5 +0,0 @@
-import pywhatkit as whaik;
-
-whaik.sendwhatmsg("+5493413762843", "prende la tele!", 23, 16, tab_close=True);
-#Syntax: pywhatkit.sendmsg(“receiver’s mobile number”, “message to be sent”, hours, minutes)
-
